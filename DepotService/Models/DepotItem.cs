6815213a782efe2949a39b59e8e15c30e57f1026
Khulud21 <145673099+Khulud21@users.noreply.github.com>
2026-03-02 13:42:41 +0100

    Updated DepotItem.cs

--- DepotService/Models/DepotItem.cs
+++ DepotService/Models/DepotItem.cs
@@ -7,12 +7,63 @@ namespace DepotService.Models
     public class DepotItem : INotifyPropertyChanged
     {
         private bool _isSelected;
+        private string _computer = "";
+        private string _domain = "";
+        private string _status = "";
+        private string _information = "";
 
-        public string Computer { get; set; } = "";
-        public string Domain { get; set; } = "";
-        public string Status { get; set; } = "";
-        public DateTime? LastCheck { get; set; }
-        public string? LastJobName { get; set; }
+        public string Computer
+        {
+            get => _computer;
+            set
+            {
+                if (_computer != value)
+                {
+                    _computer = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Domain
+        {
+            get => _domain;
+            set
+            {
+                if (_domain != value)
+                {
+                    _domain = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Status
+        {
+            get => _status;
+            set
+            {
+                if (_status != value)
+                {
+                    _status = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(JobResult));
+                }
+            }
+        }
+
+        public string Information
+        {
+            get => _information;
+            set
+            {
+                if (_information != value)
+                {
+                    _information = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
         public bool IsSelected
         {
@@ -26,6 +77,9 @@ namespace DepotService.Models
                 }
             }
         }
+
+        public DateTime? LastCheck { get; set; }
+        public string? LastJobName { get; set; }
 
         public string JobName => LastJobName ?? "Kein Job";
 
