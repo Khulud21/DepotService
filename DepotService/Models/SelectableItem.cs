fa7713729a9f41175674bc2eae00aabb9ed2de1b
Khulud21 <145673099+Khulud21@users.noreply.github.com>
2026-03-05 20:50:54 +0100

    Updated SelectableItem.cs

--- DepotService/Models/SelectableItem.cs
+++ DepotService/Models/SelectableItem.cs
@@ -5,40 +5,29 @@ namespace DepotService.Models
 {
     public class SelectableItem : INotifyPropertyChanged
     {
-        private string _name = "";
+        private string _name;
         private bool _isSelected;
+
+        public SelectableItem(string name, bool isSelected = true)
+        {
+            _name = name;
+            _isSelected = isSelected;
+        }
 
         public string Name
         {
             get => _name;
-            set
-            {
-                if (_name != value)
-                {
-                    _name = value;
-                    OnPropertyChanged();
-                }
-            }
+            set { if (_name != value) { _name = value; OnPropertyChanged(); } }
         }
 
         public bool IsSelected
         {
             get => _isSelected;
-            set
-            {
-                if (_isSelected != value)
-                {
-                    _isSelected = value;
-                    OnPropertyChanged();
-                }
-            }
+            set { if (_isSelected != value) { _isSelected = value; OnPropertyChanged(); } }
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
