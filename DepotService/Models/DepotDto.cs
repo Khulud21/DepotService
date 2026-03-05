58050f2cf98a6cd801adcf27015bdfb136b3c0b3
Khulud21 <145673099+Khulud21@users.noreply.github.com>
2026-03-05 19:48:27 +0100

    Updated DepotDto.cs

--- DepotService/Models/DepotDto.cs
+++ DepotService/Models/DepotDto.cs
@@ -1,77 +1,16 @@
 using System;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 
 namespace DepotService.Models
 {
-    public class DepotDto : INotifyPropertyChanged
+    public class DepotDto
     {
-        private bool _isSelected;
-        private int _status;
-
         public int Id { get; set; }
         public string Computer { get; set; } = "";
         public string Domain { get; set; } = "";
         public DateTime? LastCheck { get; set; }
-
-        public int Status
-        {
-            get => _status;
-            set
-            {
-                if (_status != value)
-                {
-                    _status = value;
-                    OnPropertyChanged();
-                    OnPropertyChanged(nameof(StatusText));
-                    OnPropertyChanged(nameof(StatusIcon));
-                    OnPropertyChanged(nameof(StatusDisplay));
-                }
-            }
-        }
-
+        public int Status { get; set; }
         public string? Info { get; set; }
         public DateTime? CreatedTime { get; set; }
         public string? DepotSyncId { get; set; }
-
-        public bool IsSelected
-        {
-            get => _isSelected;
-            set
-            {
-                if (_isSelected != value)
-                {
-                    _isSelected = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public string StatusText => Status switch
-        {
-            0 => "Waiting",
-            1 => "Running",
-            2 => "Success",
-            3 => "Error",
-            _ => "Unknown"
-        };
-
-        public string StatusIcon => Status switch
-        {
-            0 => "⏳",
-            1 => "🔄",
-            2 => "✅",
-            3 => "❌",
-            _ => "❓"
-        };
-
-        public string StatusDisplay => $"{StatusIcon} {StatusText}";
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
     }
 }
