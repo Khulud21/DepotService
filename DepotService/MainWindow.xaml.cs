66ab2d9c03ac017375eb088134e02c80f789cd73
Khulud21 <145673099+Khulud21@users.noreply.github.com>
2026-03-05 21:01:30 +0100

    Updated MainWindow.xaml.cs

--- DepotService/MainWindow.xaml.cs
+++ DepotService/MainWindow.xaml.cs
@@ -1,7 +1,4 @@
 using System;
-using System.ComponentModel;
-using System.IO;
-using System.Threading.Tasks;
 using System.Windows;
 using Microsoft.Extensions.Configuration;
 using DepotService.Data;
@@ -9,68 +6,27 @@ using DepotService.ViewModels;
 
 namespace DepotService
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
         private readonly MainViewModel _vm;
-        private bool _filterComputerEnabled;
-        private bool _filterJobNameEnabled;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            // appsettings.json aus Projektverzeichnis lesen
             var builder = new ConfigurationBuilder()
                 .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
             var configuration = builder.Build();
-            var conn = configuration.GetConnectionString("DefaultConnection");
-            if (string.IsNullOrWhiteSpace(conn))
-            {
-                throw new InvalidOperationException("Connection string 'DefaultConnection' fehlt in appsettings.json.");
-            }
+            var conn = configuration.GetConnectionString("DefaultConnection")
+                       ?? throw new InvalidOperationException("Connection string fehlt.");
 
             var repo = new EmpirumRepository(conn);
             _vm = new MainViewModel(repo);
             DataContext = _vm;
 
             Loaded += async (_, __) => await _vm.InitializeAsync();
-        }
-
-        public bool FilterComputerEnabled
-        {
-            get => _filterComputerEnabled;
-            set
-            {
-                if (_filterComputerEnabled == value) return;
-                _filterComputerEnabled = value;
-                OnPropertyChanged(nameof(FilterComputerEnabled));
-                // TODO: Filterlogik auslösen (z. B. ViewModel/DB-Abfrage)
-            }
-        }
-
-        public bool FilterJobNameEnabled
-        {
-            get => _filterJobNameEnabled;
-            set
-            {
-                if (_filterJobNameEnabled == value) return;
-                _filterJobNameEnabled = value;
-                OnPropertyChanged(nameof(FilterJobNameEnabled));
-                // TODO: Filterlogik auslösen (z. B. ViewModel/DB-Abfrage)
-            }
-        }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-        protected void OnPropertyChanged(string name) =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-
-        private void DataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-
         }
     }
 }
