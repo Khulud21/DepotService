9a129272b22c8e7739058858f6b916b63d0c8556
Khulud21 <145673099+Khulud21@users.noreply.github.com>
2026-03-05 19:49:56 +0100

    Added SelectableItem.cs

---
+++ DepotService/Models/SelectableItem.cs
@@ -0,0 +1,44 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DepotService.Models
+{
+    public class SelectableItem : INotifyPropertyChanged
+    {
+        private string _name = "";
+        private bool _isSelected;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                if (_isSelected != value)
+                {
+                    _isSelected = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
